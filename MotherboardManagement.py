28925056c5fcd0b90f77ce4d73b59f95f10de177
JoeYu <y1850038166@163.com>
2020-05-24 02:02:20 -0400

    Better formatting, much better comments

--- MotherboardManagement.py
+++ MotherboardManagement.py
@@ -3,7 +3,7 @@ import csv
 import os
 import sys
 
-listname = 'M.csv'
+list_name = 'M.csv'  # The file name that the program uses
 
 
 # Clear console screen
@@ -18,13 +18,13 @@ def clear():
 
 
 # Remove empty lines in the file to avoid error in processing
-# DO NOT RUN IF UNSURE THE FILE EXIST, FILE CHECK REQUIRED
+# DO NOT RUN IF UNSURE THE FILE EXIST, FILE CHECK REQUIRED, WILL THROW ERROR IF FILE DOESN'T EXIST
 def remove_empty():
-    with open(listname, 'r') as f:
-        lines = f.readlines()
-    with open(listname, 'w') as f:
-        lines = filter(lambda x: x.strip(), lines)
-        f.writelines(lines)
+    with open(list_name, 'r') as f:
+        lines = f.readlines()  # Read all the lines
+    with open(list_name, 'w') as f:
+        lines = filter(lambda x: x.strip(), lines)  # remove all blank lines by filtering
+        f.writelines(lines)  # write it all back
 
 
 class Board:
@@ -40,46 +40,47 @@ class Board:
         try:
             self.brought = float(input('Brought Price   :$'))
             self.sold = float(input('Sold Price      :$'))
-        except ValueError:
+        except ValueError:  # Bruh who is putting strings here, I hope it's a accident
             print('Bruh, please input some numbers!')
 
     def gen_profit(self):
-        self.profit = float(self.sold - (1.23 * self.brought + 15.00))
+        self.profit = float(self.sold - (1.23 * self.brought + 15.00))  # Applying formula
         return self.profit
 
     def write_to_csv(
             self):
         # This writes to the CSV file a MOTHERBOARD_NAME, PRICE BOUGHT, PRICE SOLD and automatically
         # computes PROFIT and puts it in too.
-        mode = 'a' if os.path.exists(listname) else 'w'
-        with open(listname, mode, encoding="utf-8") as f:
-            Titles = ["Board", "Bought Price", "Sold Price", "~Profit"]
-            writer = csv.DictWriter(f, fieldnames=Titles)
+        mode = 'a' if os.path.exists(list_name) else 'w'
+        with open(list_name, mode, encoding="utf-8") as f:
+            titles = ["Board", "Bought Price", "Sold Price", "~Profit"]
+            writer = csv.DictWriter(f, fieldnames=titles)
 
             writer.writerow({"Board": self.name, "Bought Price": self.brought, "Sold Price": self.sold,
-                             "~Profit": int(float(self.gen_profit()) * 100 + 0.5) / 100})
+                             "~Profit": "{0:.2f}".format(self.gen_profit())})
 
         # The program above works, but write bizarre empty line to the end of the
         # file, removing them now
         # If the cause of that issue is found and fixed, this line and the function can be removed safely
+        # TODO: Solving this problem will improve speed if dealing with very large file.
         remove_empty()
 
 
 # Calculate total profit from file
 def calc_total():
     # Perform file check
-    if os.path.exists(listname):
-        # Removes empty lines in the file to avoid runtime error, can be safely removed if the bug is
-        # fixed
+    if os.path.exists(list_name):
+        # Removes empty lines in the file to avoid runtime error, can be safely removed if the issue mentioned on
+        # line 62 is fixed (maybe, leaving it here will help with blank lines unintentionally created)
         remove_empty()
-        with open(listname, 'r', encoding="utf-8") as f:
+        with open(list_name, 'r', encoding="utf-8") as f:
             excel = csv.reader(f)
-            Total_Profit = 0.00
+            total_profit = 0.00
             try:
-                for line in excel:
-                    Total_Profit += float(line[3])
-                    print(("$") + str(int(float(Total_Profit) * 100 + 0.5) / 100) + " of approximate total profit")
-            except ValueError:
+                for line in excel:  # Going over each line
+                    total_profit += float(line[3])  # Add profit from each line's fourth column
+                print("$" + "{0:.2f}".format(float(total_profit)) + " of approximate total profit")
+            except ValueError:  # If cannot covert to float, notify the user
                 print('Value Error!\nA string is found at where a number is supposed to go in. \n Check your '
                       'file and make sure it is formatted correctly.')
 
@@ -87,23 +88,23 @@ def calc_total():
                 print('Runtime Error:', e)
                 print('The program will now continue. If the error persist, contact the developer')
                 print('Check your file and make sure it is formatted correctly.')
-    else:
+    else:  # The file is not there.
         print('File is empty. Please add a board to continue')
 
 
-# Calculate breakeven by the formula
-def breakeven(x):
-    return int((1.23 * float(x) + 15) * 100 + 0.5) / 100.0
+# Calculate break even by the formula
+def break_even(x):
+    return "{0:.2f}".format(1.23 * float(x) + 15)  # Applying formula, returns string
 
 
 # Simply Print the list
 def print_list():
     # Perform file check
-    if os.path.exists(listname):
-        # Removes empty lines in the file to avoid runtime error, can be safely removed if the bug is
-        # fixed
+    if os.path.exists(list_name):
+        # Removes empty lines in the file to avoid runtime error, can be safely removed if the issue mentioned on
+        # line 62 is fixed (maybe, leaving it here will help with blank lines unintentionally created)
         remove_empty()
-        with open(listname, 'r', encoding="utf-8") as f:
+        with open(list_name, 'r', encoding="utf-8") as f:
             excel = csv.reader(f)
             try:
                 for line in excel:
@@ -114,6 +115,7 @@ def print_list():
                 print('The program will now continue. If the error persist, contact the developer')
 
 
+# Add two parts of the welcome message, will be put together added with the list_name
 menu_message1 = '''Welcome to MotherboardManagement
     By Isaac Chen and Joe Yu
                 
@@ -129,17 +131,16 @@ menu_message2 = '''
 [6] Exit'''
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        listname = sys.argv[1]
+    if len(sys.argv) > 1:  # If an argument is passed, set the list_name
+        list_name = sys.argv[1]
     while True:
         clear()
-        print(menu_message1 + listname + menu_message2)
+        print(menu_message1 + list_name + menu_message2)
         operation = input()
-        if operation == '1':
+        if operation == '1':  # Process a new board
             b = Board()
             b.init()
-            b.gen_profit()
-            print("Profit is $" + str(b.profit))
+            print("Profit is $" + str(b.gen_profit()))
             next_step = input('Write to file? Y/N\n')  # Prompting user to write to file
             if next_step == 'y' or next_step == 'Y':
                 b.write_to_csv()
@@ -147,10 +148,10 @@ if __name__ == '__main__':
 
         elif operation == '2':  # This takes 1 input, and returns the price it must be sold at to break even.
             x = input('Brought Price		:')
-            print("$" + (str(breakeven(x)) + " is the selling price needed to break even"))
+            print("$" + (break_even(x) + " is the selling price needed to break even"))
             input('Press Enter To Continue')
 
-        elif operation == '3':
+        elif operation == '3':  # Simply output the list
             print_list()
             input('Press Enter To Continue')
 
@@ -158,13 +159,13 @@ if __name__ == '__main__':
             calc_total()
             input('Press Enter To Continue')
 
-        elif operation == '5':
-            listname = input('Please enter new filename, include suffix (.csv):')
+        elif operation == '5':  # Rename list_name
+            list_name = input('Please enter new filename, include suffix (.csv):')
             input('Press Enter To Continue')
 
-        elif operation == '6':
+        elif operation == '6':  # Bye
             print('Have a good day!')
             break
-        else:
-            print("Wrong Operation, Please input 1-5")
+        else:  # Input out of range
+            print("Wrong Operation, Please input 1-6")
             input('Press Enter To Continue')
